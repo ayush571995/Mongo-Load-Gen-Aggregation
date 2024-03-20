33f08daa9b59a1c7771a073bf504456414be78c2
ayush571995 <ayush571995@outlook.com>
2024-03-21 03:47:10 +0530

    allowing locust to call with new api

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,20 +1,34 @@
-import os,json
+import os
 from locust import FastHttpUser, task, between
 
+# Get the input type from environment variables
+input_type = os.getenv('INPUT_TYPE', 'aggregate')
 
 class MyUser(FastHttpUser):
 
-    @task
-    def post_task(self):
-        # Reading POST parameters from environment variables
-        # post_params = os.getenv("POST_PARAMS", '{"key": "default_value"}')
+    def post_aggregate_generic(self):
         json_data = {
             "opName": os.getenv("OP_NAME"),
             "expression": os.getenv("EXPRESSION"),
             "expectedFieldName": os.getenv("RESULT_FIELD"),
             "collectionName": os.getenv("COLLECTION")
         }
-        # Converting the string representation of the dictionary to an actual dictionary
-        # post_data = json.loads(post_params)
-
         self.client.post("aggregate/generic", json=json_data)
+
+    def post_aggregate_pipeline(self):
+        json_data = {
+            "opName": os.getenv("OP_NAME"),
+            "expression": os.getenv("EXPRESSION"),
+            "expectedFieldName": os.getenv("RESULT_FIELD"),
+            "collectionName": os.getenv("COLLECTION")
+        }
+        self.client.post("aggregate/pipeline", json=json_data)
+
+    @task
+    def execute_task_based_on_input(self):
+        if input_type == 'aggregate':
+            self.post_aggregate_generic()
+        elif input_type == 'pipeline':
+            self.post_aggregate_pipeline()
+        else:
+            print(f"Unknown input_type: {input_type}")
